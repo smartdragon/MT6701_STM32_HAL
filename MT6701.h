4471c8b06c026977a957391a22949fa3643d1c91
TTXBEB <hclegg374@outlook.com>
2022-06-28 20:30:50 +0800

    将角度读取改为浮点类型 增加SSI方式读取信息

--- MT6701.h
+++ MT6701.h
@@ -11,6 +11,15 @@
 #define ReadAddress1    0X03        //数据高位寄存器地址
 #define ReadAddress2    0X04        //数据低位寄存器地址
 
-uint32_t ReadAngleIIC(void);
+#define Loss_of_Track       4
+#define Button_Detected     3
+#define Field_Weak          2
+#define Field_Strong        1
+#define Normal              0
+
+double ReadAngleIIC(void);
+double ReadDataSSI(uint8_t mode);
+void Delay_us(uint32_t nus);
+void Delay_ms(uint16_t nms);
 
 #endif //MT6701_MT6701_H
